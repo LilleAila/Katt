6bbe78db4a9a4af61bb0b010dde7e289756b9b58
Lille Aila <67327023+LilleAila@users.noreply.github.com>
2020-07-30 17:45:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-function getRandomSize(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
-
 $('document').ready(function() {
   for (var i = 0; i < 3; i++) {
-    var width = getRandomSize(200, 400);
-    var height =  getRandomSize(200, 400);
     $.ajax({
       url: 'https://dog.ceo/api/breeds/image/random',
       type: 'get',
       dataType: 'json',
       success: function(data) {
-        $('#photos').append('<img src="' + data.message + '" style="width:' + width + '; height:' + height + '">');
+        $('#photos').append('<img src="' + data.message + '">');
       console.log(data.message);
       },
       error: function() {
