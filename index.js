264cdce289c7cf49fe150140ac458d62fd1832b7
Lille Aila <67327023+LilleAila@users.noreply.github.com>
2020-07-30 17:57:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 $('document').ready(function() {
   for (var i = 0; i < 3; i++) {
     $.ajax({
-      url: 'https://dog.ceo/api/breeds/image/random',
+      url: 'https://api.thecatapi.com/v1/images/search',
       type: 'get',
       dataType: 'json',
       success: function(data) {
         $('#photos').append('<img src="' + data.message + '">');
-      console.log(data.message);
+        console.log(data);
       },
       error: function() {
         console.log(':(')
